1124e50d3f8ec9d14f93b53d943edb19752f6b57
devnoom <archilmenabde@yahoo.com>
2024-06-05 15:14:24 +0400

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by MacBook Air on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
